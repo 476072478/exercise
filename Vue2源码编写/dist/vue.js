6aee477d018aaa2a8a21c5e985bcf06e5e2e87ef
小唐 <476072478@qq.com>
2023-06-06 09:19:38 +0800

    feat: 实习完整的diff算法

--- Vue2源码编写/dist/vue.js
+++ Vue2源码编写/dist/vue.js
@@ -956,7 +956,9 @@
     // 静态方法
     Vue.options = {};
     Vue.extend = function (options) {
-      function Sub() {}
+      function Sub() {
+        this._init_();
+      }
       Sub.prototype = Object.create(Vue.prototype); //  Sub.prototype._proto_ =  Vue.prototype
       Sub.options = options; // 保存用户传递的选项
       return Sub;
