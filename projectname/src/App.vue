86a30e476350600e495c82ee188ab130fe63d079
唐帝 <476072478@qq.com>
2023-12-05 16:05:45 +0800

    feat: 更改

---
+++ projectname/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ nameObj.name }}
+    {{ nameObj.title }}
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const nameObj = reactive({
+  name: "小唐",
+  title: "xxxx",
+});
+setTimeout(() => {
+  nameObj.name = '小林'
+}, 2000);
+</script>
+
+<style></style>
